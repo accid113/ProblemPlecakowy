f446b84b41a782432544629163b50dc06abee637
Hubert Kaczmarek <twojemail@example.com>
2024-10-28 19:19:39 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+def backpack(values, weights, capacity):
+    n = len(values)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    #tworzenie tablic
+    for i in range(1, n + 1):
+        for c in range(1, capacity + 1):
+            if weights[i - 1] <= c:
+                dp[i][c] = max(dp[i - 1][c], dp[i - 1][c - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][c] = dp[i - 1][c]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    selected_items = []
+    c = capacity
+    for i in range(n, 0, -1):
+        if dp[i][c] != dp[i - 1][c]:
+            selected_items.append(i - 1)
+            c -= weights[i - 1]
+
+    #wydruk
+    return dp[n][capacity], selected_items
+
+#słownik
+items = {
+    "śpiwór": (10, 10),
+    "szczoteczka do zębów": (10, 1),
+    "pasta do zębów": (5, 1),
+    "koc": (8, 7),
+    "bluza": (9, 5),
+    "woda": (9, 2),
+    "ręcznik": (5, 3),
+    "tablet": (8, 2),
+    "jedzenie": (10, 5),
+    "buty": (7, 3),
+    "latarka": (6, 3),
+    "zegarek": (4, 2),
+    "konsola": (6, 4),
+    "piłka": (4, 3),
+    "płyn": (8, 2)
+}
+
+#tablica z wartosciami
+values = [item[0] for item in items.values()]
+weights = [item[1] for item in items.values()]
+
+#pojemnosc plecaka
+capacity = 18
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+max_value, selected_item_indices = backpack(values, weights, capacity)
+print("Maksymalna wartość:", max_value)
+print("Wybrane przedmioty:")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for index in selected_item_indices:
+    item_name = list(items.keys())[index]
+    print(f"- {item_name}")
+
